854bfeb030d7a5fd84191b51630d7ba56281c424
zb254 <zb254@163.com>
2024-10-08 13:52:37 +0800

    1.0.0
    add

---
+++ example/ios/Pods/Target Support Files/image_selector/image_selector-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_image_selector : NSObject
+@end
+@implementation PodsDummy_image_selector
+@end
---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<image_selector/ImagesPickerPlugin.h>)
+#import <image_selector/ImagesPickerPlugin.h>
+#else
+@import image_selector;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [ImagesPickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"ImagesPickerPlugin"]];
+}
+
+@end
